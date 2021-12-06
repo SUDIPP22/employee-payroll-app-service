f8d985270b7190a6d578ec15c26be0121b7690a9
Sudip Panja <panja370@gmail.com>
2021-12-07 02:42:56 +0530

    Added : Dependencies and application properties required for employee payroll application

---
+++ src/main/java/com/bridgelabz/EmployeePayrollAppApplication.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Purpose : To demonstrate employee payroll application
+ *
+ * @author SUDIP PANJA
+ * @version : 0.0.1-SNAPSHOT
+ * @since 2021-12-06
+ */
+@SpringBootApplication
+public class EmployeePayrollAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppApplication.class, args);
+    }
+
+}
