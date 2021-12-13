e9f78f9a5ffbe44108f11f822ef55036d783d93c
Sudip Panja <panja370@gmail.com>
2021-12-14 00:22:27 +0530

    Modified : Class name

--- src/test/java/com/bridgelabz/EmployeePayrollAppApplicationTests.java
+++ src/test/java/com/bridgelabz/EmployeePayrollAppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeDetailsDtoPayrollAppApplicationTests {
+class EmployeePayrollAppApplicationTests {
 
 	@Test
 	void contextLoads() {
