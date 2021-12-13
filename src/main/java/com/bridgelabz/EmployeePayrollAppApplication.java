b08bdd54a3c27381e59ac4ad010b269c1847ded4
Sudip Panja <panja370@gmail.com>
2021-12-14 00:21:30 +0530

    Added : EnableEncryptableProperties annotation to enable the encryption prorperties

--- src/main/java/com/bridgelabz/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollAppApplication.java
@@ -1,5 +1,6 @@
 package com.bridgelabz;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2021-12-06
  */
 @SpringBootApplication
+@EnableEncryptableProperties
 public class EmployeePayrollAppApplication {
 
     public static void main(String[] args) {
