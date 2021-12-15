d1ad0af2ec3c6f7a8583cbbcf4b830e3f286b6ef
Sudip Panja <panja370@gmail.com>
2021-12-16 01:48:49 +0530

    Modified : Department field to list of department

--- src/main/java/com/bridgelabz/dto/EmployeeDetailsDto.java
+++ src/main/java/com/bridgelabz/dto/EmployeeDetailsDto.java
@@ -3,6 +3,7 @@ package com.bridgelabz.dto;
 import lombok.Data;
 
 import javax.validation.constraints.*;
+import java.util.List;
 
 /**
  * Purpose : To invoke the data from client
@@ -32,7 +33,7 @@ public class EmployeeDetailsDto {
 
     @NotNull(message = "Department name should not be empty")
     @Size(message = "Department name Should be within 50 letters", max = 50)
-    private String department;
+    private List<String> departments;
 
     @NotNull(message = "Notes should not be empty")
     @Size(message = "Notes should be within 150 letters", max = 150)
@@ -43,6 +44,6 @@ public class EmployeeDetailsDto {
 
     @NotNull(message = "Joining date should not be empty")
     @Pattern(regexp = "^(1[0-2]|0[1-9])/(3[01]|[12][0-9]|0[1-9])/[0-9]{4}$",
-            message = "Date should be maintain the format : mm-dd-yyyy")
+            message = "Date should be maintain the format : mm/dd/yyyy")
     private String joiningDate;
 }
