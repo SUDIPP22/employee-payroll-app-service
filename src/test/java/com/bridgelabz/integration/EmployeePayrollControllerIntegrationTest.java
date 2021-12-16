79654317aa8ec14b7c431366277e26aa5fa9dc21
Sudip Panja <panja370@gmail.com>
2021-12-17 01:25:58 +0530

    Added : Integration controller test cases if validation of employee data's are going to fail

--- src/test/java/com/bridgelabz/integration/EmployeePayrollControllerIntegrationTest.java
+++ src/test/java/com/bridgelabz/integration/EmployeePayrollControllerIntegrationTest.java
@@ -47,6 +47,18 @@ public class EmployeePayrollControllerIntegrationTest {
     }
 
     @Test
+    void addEmployeeDataTest_WhenValidationFailed_ShouldGetBadRequest() throws Exception {
+        when(employeePayrollService.addEmployeeEntities(any())).thenReturn(new ResponseDto());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/employee/detail")
+                        .content("{\"firstName\":\"sudip\",\"lastName\":\"panja\",\"gender\":\"Male\"," +
+                                "\"salary\":800000,\"departments\":[\"JavaDeveloper\",\"Manager\"]," +
+                                "\"notes\":\"Reliable\",\"imagePath\":\"image1.jpg\",\"joiningDate\":\"12/16/2021\"}")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     void getEmployeeDetailsTest() throws Exception {
         when(employeePayrollService.getAllEmployeeData()).thenReturn(new ArrayList<>());
         mockMvc.perform(MockMvcRequestBuilders.get("/employee/details"))
@@ -81,6 +93,28 @@ public class EmployeePayrollControllerIntegrationTest {
                                 "\"notes\":\"Reliable\",\"imagePath\":\"image2.jpg\",\"joiningDate\":\"12/16/2021\"}")
                         .contentType(MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isAccepted());
+    }
+
+    @Test
+    void updateEmployeeDetailsTest_WhenValidationFailed_ShouldGetBadRequest() throws Exception {
+        EmployeeDetailsDto employeeDetailsDto = new EmployeeDetailsDto();
+        employeeDetailsDto.setFirstName("Sudip");
+        employeeDetailsDto.setLastName("Panja");
+        employeeDetailsDto.setGender("Male");
+        employeeDetailsDto.setSalary(600000.00);
+        employeeDetailsDto.setDepartments(List.of("Electrical"));
+        employeeDetailsDto.setNotes("Reliable,Adaptable,Self-Manageable,Communicative");
+        employeeDetailsDto.setImagePath("image1.jpg");
+        employeeDetailsDto.setJoiningDate("12/12/2021");
+        int employeeId = 1;
+        when(employeePayrollService.updateEmployeeData(employeeId, employeeDetailsDto)).thenReturn(new ResponseDto());
+        mockMvc.perform(MockMvcRequestBuilders
+                        .put("/employee/detail/1")
+                        .content("{\"firstName\":\"sudip\",\"lastName\":\"Panja\",\"gender\":\"male\"," +
+                                "\"salary\":800000,\"departments\":[\"JavaDeveloper\",\"Manager\"]," +
+                                "\"notes\":\"Reliable\",\"imagePath\":\"image2.jpg\",\"joiningDate\":\"12/16/2021\"}")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isBadRequest());
     }
 
     @Test
